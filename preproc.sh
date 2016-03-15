928f76fef38a1b934be861be35bca8519c4e7d55
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-03-15 14:36:34 +1100

    Still experimenting with preprocessor...

--- preproc.sh
+++ preproc.sh
@@ -11,9 +11,10 @@ do
 
     filename=$(basename "$vocab")  # vocab/1.xml -> 1.xml
     noextension="${filename%.*}"   # 1.xml -> 1
-
-    regexp="s/<!--$noextension-->/\`cat vocabs\/$noextension.xml\`/ge"
-    perl -pe "$regexp" -i module.xml
+    sed -i -e "s#<!--$noextension-->#r vocabs/$noextension.xml" module.xml
+#    sed -i -e "s/<!--"
+#    regexp="s/<!--$noextension-->/\`cat vocabs\/$noextension.xml\`/ge"
+#    perl -pe "$regexp" -i module.xml
 done
 
 echo "========================================================================="
