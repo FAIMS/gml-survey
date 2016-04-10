d6aefc7300850614c5c137bd5d816db09af5d367
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-04-10 19:51:04 +1000

    Forgot to update postproc.sh as part of 3663143a97ad9ac73b195fd71cec06c57b8ef890

--- postproc.sh
+++ postproc.sh
@@ -6,11 +6,16 @@ string="
   String autoNumDest = \"\";
   autoNumDest = \"Transect\/Transect\/Transect_ID\";
   incAutoNum(autoNumDest);"
-replacement="
-  String autoNumDest = \"\";
-  autoNumDest = \"Transect\/Transect\/Transect_ID\";
-  incAutoNum(autoNumDest);
+replacement="$string
   copyDeviceId();"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
+string="
+onClickLoginLogin () {
+  // TODO: Add some things which should happen when this element is clicked
+  newTab(\"Control\", true);
+}"
+replacement=""
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
 rm ui_logic.bsh.original
